9a7862430681cccd78b347c8c391c85ec6868760
Ziad Hegazi <ziadhegazi@hotmail.com>
2021-09-27 18:08:46 +0200

    using Socket.io

---
+++ Public/chat.js
@@ -0,0 +1,2 @@
+// make connection
+const socket = io.connect("http://localhost:8000");
--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const socket = require("socket.io");
 
 const app = express();
-app.listen(8000, () => {
+const server = app.listen(8000, () => {
     console.log("Server started on port 8000");
 });
 
 // static files
 app.use(express.static("Public"));
+
+// socket setup
+const io = socket(server);
+
+io.on("connection", (socket) => {
+    console.log("Connection made");
+});
