5b21facc3b015e426ca6784c68a5d385972767dd
Ziad Hegazi <ziadhegazi@hotmail.com>
2021-09-27 19:07:20 +0200

    Broadcasting messages

--- Public/chat.js
+++ Public/chat.js
@@ -5,14 +5,24 @@ const socket = io.connect("http://localhost:8000");
 const message = document.getElementById("message"),
     handle = document.getElementById("handle"),
     btn = document.getElementById("send"),
-    output = document.getElementById("output");
+    output = document.getElementById("output"),
+    feedback = document.getElementById("feedback");
 
 // emit events
 btn.addEventListener("click", () => {
     socket.emit("chat", { handle: handle.value, message: message.value });
 });
 
+message.addEventListener("keypress", () => {
+    socket.emit("typing", handle.value);
+});
+
 // Listen for events
 socket.on("chat", (data) => {
     output.innerHTML += `<p><strong>${data.handle}:</strong> ${data.message}</p>`;
+    feedback.innerHTML = ``;
+});
+
+socket.on("typing", (data) => {
+    feedback.innerHTML = `<p><em>${data} is typing ...</em></p>`;
 });
